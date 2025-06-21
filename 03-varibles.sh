6b17363207783690f9c049c53051805d39d327db
swamy vasse <swamyvasse@gmail.com>
2025-06-21 16:59:10 +0530

    commited changes

---
+++ 03-varibles.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=&2
+
+echo "$PERSON1: Hi $PERSON2, GoodMorning!"
+echo "$PERSON2: Hi $PERSON1, GoodMorning!"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am fine, How are you $PERSON1!"
