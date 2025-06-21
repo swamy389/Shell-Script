4d94bb296681394761fdcc76605e34c94dcc67d6
swamy vasse <swamyvasse@gmail.com>
2025-06-21 16:39:29 +0530

    commited changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "i am learning devops with aws"
+
+echo "Suresh: Hi Ramesh, GoodMorning!"
+echo "Ramesh: Hi Suresh, GoodMorning!"
+echo "Suresh: How are you Ramesh"
+echo "Ramesh: I am fine, How are you Suresh!"
