b85ca2179f3db2846c50a55af769c1c2c8c33c2d
swamy vasse <swamyvasse@gmail.com>
2025-06-22 11:24:37 +0530

    commited changes

--- 14-functions.sh
+++ 14-functions.sh
@@ -7,14 +7,18 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE () {
 
 if [ $1 -ne 0 ]
 then 
-   echo "$2 ... FAILURE"
+   echo  -e "$2 ... $R FAILURE $N"
    exit 1
 else 
-   echo "$2 ... SUCCESS"
+   echo  -e "$2 ... $G SUCCESS $N"
 fi
 
 }
