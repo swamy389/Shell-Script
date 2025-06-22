f4eb5b72b2015c9a301a68bad612ff05cb5dcc07
swamy vasse <swamyvasse@gmail.com>
2025-06-22 12:04:13 +0530

    commited changes

--- 17-loops.sh
+++ 17-loops.sh
@@ -2,16 +2,16 @@
 
 USERID=$(id -u)
 R="\e[31m"
-N=\"e[0m"
+N="\e[0m"
 
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ];
 then 
    echo -e "$R ERROR:: run the script with root access $N"
    exit 1
 fi
 
-
+#huhhuu
 for i in $@
 do 
 
