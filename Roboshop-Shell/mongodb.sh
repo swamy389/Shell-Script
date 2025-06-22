68383433160cc5506719fcabcca9ea18f4738577
swamy vasse <swamyvasse@gmail.com>
2025-06-22 13:23:51 +0530

    commited changes

---
+++ Roboshop-Shell/mongodb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+#/home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then 
+   echo "$R ERROR:: Please run this script with root access $N"
+   exit 1
+# else
+#     echo "INFO:: this is root user"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then 
+       echo -e "$2...$R FAILURE $N"
+       exit 1
+    else
+       echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+cp mongod.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+VALIDATE $? "Copied MongoDb repo into yum.repos.d"
+
+VALIDATE $? "Installation of MongoDB"
+
+systemctl enable mongod
+
+VALIDATE $? "Enable Mongod"
+
+systemctl start mongod
+
+VALIDATE $? "Start Mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOGFILE
+
+VALIDATE $? "Enable Mongod Conf"
+
+systemctl restart mongod &>>$LOGFILE
+
+VALIDATE $? "Restarting MongoDB"
