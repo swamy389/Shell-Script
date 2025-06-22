40370cbc905a718958f00cdaa89cf5d0ac7abf53
swamy vasse <swamyvasse@gmail.com>
2025-06-22 11:59:53 +0530

    commited changes

--- 17-loops.sh
+++ 17-loops.sh
@@ -1,7 +1,20 @@
 #!/bin/bash 
 
+USERID=$(id -u)
+R="\e[31m"
+N=\"e[0m"
+
+
+if [ $USERID -ne 0 ]
+then 
+   echo  -e "$R ERROR:: run the script with root access $N"
+   exit 1
+fi
+
 
 for i in $@
 do 
-  yum install $i -y 
-done 
+
+  yum install $i -y
+done
+
