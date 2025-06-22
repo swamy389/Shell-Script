85f93ed17f2fae03aa1655197b790c76933239ff
swamy vasse <swamyvasse@gmail.com>
2025-06-22 15:21:21 +0530

    commited changes

--- Roboshop-Shell/mongodb.sh
+++ Roboshop-Shell/mongodb.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 DATE=$(date +%F)
 LOGSDIR=/tmp
-#/home/centos/shellscript-logs/script-name-date.log
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
@@ -13,26 +12,27 @@ N="\e[0m"
 Y="\e[33m"
 
 if [ $USERID -ne 0 ];
-then 
-   echo "$R ERROR:: Please run this script with root access $N"
-   exit 1
-# else
-#     echo "INFO:: this is root user"
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ];
-    then 
-       echo -e "$2...$R FAILURE $N"
-       exit 1
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
     else
-       echo -e "$2...$G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-VALIDATE $? "Copied MongoDb repo into yum.repos.d"
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
+
+yum install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installation of MongoDB"
 
@@ -46,8 +46,8 @@ VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 
-VALIDATE $? "Enable Mongod Conf"
+VALIDATE $? "Edited MongoDB conf"
 
-systemctl restart mongod &>>$LOGFILE
+systemctl restart mongod &>> $LOGFILE
 
-VALIDATE $? "Restarting MongoDB"
+VALIDATE $? "Restarting MonogoDB"
